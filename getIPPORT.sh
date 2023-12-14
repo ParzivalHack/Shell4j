935bd77143449a176530963c0123a49c56feb219
Tommaso Bona <piergeolo@gmail.com>
2023-12-14 13:57:18 +0100

    Update getIPPORT.sh

--- getIPPORT.sh
+++ getIPPORT.sh
@@ -6,6 +6,7 @@ read -p "Enter the URL: " url
 # Extract the domain from the URL
 domain=$(echo "$url" | awk -F[/:] '{print $4}')
 
+webserverdomain=$(dig +short "$domain" | awk '!/^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/')
 # Use dig to retrieve the IP address of the web server
 ip_address=$(dig +short "$domain" | tail -n1)
 
@@ -15,6 +16,7 @@ port=$(curl -sI "$url" | grep -i "location: http" | awk -F: '{print $3}' | tr -d
 # Display the results in a more organized way
 echo "Website Information for $url:"
 echo "-------------------------------------"
+echo "Web Server Domain: $webserverdomain"
 echo "Web Server IP Address: $ip_address"
 echo "Web Server Port: $port"
 echo "-------------------------------------"
